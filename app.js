6b099c148b1f06a0170e28504f5af4289caf8dee
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-11 22:11:11 +0500

    question are ready

---
+++ app.js
@@ -0,0 +1,78 @@
+console.log('Are you read for quiz?');
+
+var questions = [{
+    "question": 'Which function among the following lets to register a function to be invoked repeatedly after a certain time?',
+	"option1": 'setTimeout()',
+	"option2":'setTotaltime()',
+	"option3":'setInterval()',
+	"option4":'none of the mentioned',
+	"answer": 3,    
+},{
+    "question": 'To which object does the location property belong?',
+	"option1": 'Window',
+	"option2": 'Position',
+	"option3" :'Element',
+    "option4": 'Location',
+    "asnwer": 1
+},{
+
+},{
+    "question": 'What is the result of the following code snippet? <br> <pre> window.location === document.location </pre>',
+	"option1": 'False',
+	"option2": 'True',
+	"option3": '1',
+    "option4": '0',
+    "answer": 2,
+},{
+    "question": 'Which of the following is not a framework?',
+	"option1": 'jQuery',
+	"option2": '.NET',
+	"option3": 'JavaScript',
+    "option4": 'None of the mentioned',
+    "answer": 3,
+}, {
+    "question": 'Inside which HTML element do we put the JavaScript?',
+	"option1": '&lt;javascript&gt;',
+	"option2": '&lt;script&gt;',
+	"option3": '&lt;scripting&gt;',
+    "option4": '&lt;js&gt;',
+    "answer": 2,
+}, {
+    "question": 'What is the correct JavaScript syntax to change the content of the HTML element below? <br> <pre>&lt;p id="demo"&gtThis is made by Owais.&lt;/p&gt</pre>',
+	"option1": 'document.getElementByName("p").innerHTML = "Hello World!";',
+	"option2": '#demo.innerHTML = "Hello World!";',
+	"option3": 'document.getElementById("demo").innerHTML = "Hello World!";',
+    "option4": 'document.getElement("p").innerHTML = "Hello World!";',    
+    "answer": 3,
+},{
+    "question": 'What is the correct syntax for referring to an external script called "script.js"?',
+	"option1": '&lt;script src="script.js"&gt;',
+	"option2": '&lt;script href="script.js"&gt;',
+	"option3": '&lt;script name="script.js"&gt;',
+    "option4": '&lt;script link="script.js"&gt;',
+    "answer": 1,
+},{
+    "question": 'How do you write "Hello World" in an alert box?',
+	"option1": 'alert("Hello World");',
+	"option2": 'msgBox("Hello World");',
+	"option3": 'prompt("Hello World");',
+    "option4": 'alertBox("Hello World");',
+    "answer": 1
+}, {
+    "question": 'How do you create a function in JavaScript?',
+    "option1": 'function = myFunction()',
+    "option2": 'function:myFunction()',
+    "option3": 'function myFunction()',
+    "option4": 'None of the mentioned',
+    "answer": 3,
+}, {
+    "question": 'How do you call a function named "myFunction"?',
+	"option1": 'myFunction()',
+	"option2": 'call function myFunction()',
+	"option3": 'call myFunction()',
+    "option4": 'None of the mentioned',
+    "answer": 1,
+}]
+
+
+
